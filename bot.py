a9eb6cb12e8913140536b93e3322658dd18f72b8
munipka <kotmun95@yandex.ru>
2022-08-29 13:14:45 +0300

    set_command update

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.types import BotCommand
 
 from handlers.commands import register_commands
 from handlers.callbacks import register_callbacks
@@ -9,6 +10,19 @@ from handlers.register import register_state_callbacks
 from handlers.inline import register_inline
 from apps.database import create_tables
 import config
+
+
+async def set_commands(dp: Dispatcher):
+    """ set up commands for menu"""
+    bot_commands = [
+        BotCommand(command="add", description="add a wallet"),
+        BotCommand(command="list", description="list of all addresses"),
+        BotCommand(command="wallets", description="edit my wallets"),
+        BotCommand(command="clear", description="delete all data"),
+        BotCommand(command="faucet", description="faucet for test tokens"),
+        BotCommand(command="help", description="help")
+    ]
+    await dp.bot.set_my_commands(bot_commands)
 
 
 async def main():
@@ -19,6 +33,7 @@ async def main():
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
     )
     await create_tables()
+    await set_commands(dp)
     register_commands(dp)
     register_callbacks(dp)
     register_state_callbacks(dp)
